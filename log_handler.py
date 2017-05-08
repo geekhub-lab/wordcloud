5a8e8a02b6d0dd438c15200609d9c9519082b89c
mingrammer <k239507@gmail.com>
2017-05-09 01:37:23 +0900

    :tada: Initial commit for wordcloud features

---
+++ log_handler.py
@@ -0,0 +1,45 @@
+import csv
+import glob
+import os
+import sys
+from typing import List, Dict, Iterable
+
+
+def find_log_file():
+    args = sys.argv
+    if len(args) == 1:
+        list_of_chat_log = glob.glob('logs/*.csv')
+        latest_chat_log = max(list_of_chat_log, key=os.path.getctime)
+        chat_log_file = latest_chat_log
+    else:
+        chat_log_file = args[1]
+        if not os.path.isfile(chat_log_file):
+            raise FileNotFoundError(f('{chat_log_file} is not found'))
+    return chat_log_file
+
+
+def get_log_reader(log_file: str) -> Iterable[Dict[str, str]]:
+    csvfile = open(log_file, encoding='utf-8-sig')
+    reader = csv.DictReader(csvfile)
+    return reader
+
+
+def get_user_log_string(log: Dict[str, List[str]]) -> str:
+    return ' '.join(log['user_log'])
+
+
+def get_message_log_string(log: Dict[str, List[str]]) -> str:
+    return ' '.join(log['message_log'])
+
+
+def read_all_log(reader: Iterable[Dict[str, str]]) -> Dict:
+    data = {
+        'time_series_log': list(),
+        'message_log': list(),
+        'user_log': list(),
+    }
+    for line in reader:
+        data['time_series_log'].append(line['Date'])
+        data['user_log'].append(line['User'])
+        data['message_log'].append(line['Message'])
+    return data
---
+++ make_wordcloud.py
@@ -0,0 +1,26 @@
+from wordcloud import WordCloud
+
+import log_handler as lh
+
+default_wordcloud = WordCloud(width=1080,
+                              height=640,
+                              font_path='fonts/NanumGothic.ttf')
+
+
+def make_whole_wordcloud(chat_log_text: str) -> None:
+    wordcloud = default_wordcloud.generate(chat_log_text)
+    image = wordcloud.to_image()
+    image.show()
+
+
+if __name__ == '__main__':
+    chat_log_file = lh.find_log_file()
+
+    log_reader = lh.get_log_reader(chat_log_file)
+    log_data = lh.read_all_log(log_reader)
+
+    user_log_string = lh.get_user_log_string(log_data)
+    message_log_string = lh.get_message_log_string(log_data)
+
+    make_whole_wordcloud(user_log_string)
+    make_whole_wordcloud(message_log_string)
