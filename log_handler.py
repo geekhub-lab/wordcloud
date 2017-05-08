ef3f97d1a6faaaa8153efcd5d494409c0cebfae5
mingrammer <k239507@gmail.com>
2017-05-09 01:55:07 +0900

    :books: Add comments for all log handlers

--- log_handler.py
+++ log_handler.py
@@ -6,6 +6,11 @@ from typing import List, Dict, Iterable
 
 
 def find_log_file():
+    """Find kakaotalk chat log file.
+
+    If there is no argument, it will find the latest log file from 'log/' directory.
+    Or it uses the given log file.
+    """
     args = sys.argv
     if len(args) == 1:
         list_of_chat_log = glob.glob('logs/*.csv')
@@ -19,20 +24,40 @@ def find_log_file():
 
 
 def get_log_reader(log_file: str) -> Iterable[Dict[str, str]]:
-    csvfile = open(log_file, encoding='utf-8-sig')
+    """Getting a reader object for reading the log data from csv format.
+
+    Args:
+        log_file: Path of chat log file
+    """
+    csvfile = open(log_file, encoding='utf-8-sig') # Ignore the first BOM character with 'utf-8-sig'
     reader = csv.DictReader(csvfile)
     return reader
 
 
 def get_user_log_string(log: Dict[str, List[str]]) -> str:
+    """"Convert the user log list to a string.
+
+    Args:
+        log: Chat log data
+    """
     return ' '.join(log['user_log'])
 
 
 def get_message_log_string(log: Dict[str, List[str]]) -> str:
+    """"Convert the message log list to a string.
+
+    Args:
+        log: Chat log data
+    """
     return ' '.join(log['message_log'])
 
 
 def read_all_log(reader: Iterable[Dict[str, str]]) -> Dict:
+    """"Read all chat log and get the all rows corresponding to each columns.
+
+    Args:
+        reader: A file contents reader for reading the log data
+    """
     data = {
         'time_series_log': list(),
         'message_log': list(),
